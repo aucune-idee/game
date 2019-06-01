f78f0071c6f944634ae3aa1fa5dadf2389dd02c7
Kamule <benjamin.herbomez@gmail.com>
2019-06-01 23:06:48 +0200

    Fix cors

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ConfigService } from './config/config.service';
+
+let config = new ConfigService();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(8080);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: function (origin:any, callback:any) {
+          if (config.envConfig.cors.indexOf(origin) !== -1 || origin == null) {
+              callback(null, true)
+          } else {
+              callback(new Error('Not allowed by CORS'))
+          }
+      }
+    }
+  });
+  await app.listen(config.envConfig.port);
 }
 bootstrap();
