a0591c417b690f482acf9af22c2fb1fc07a9fce6
Kamule <benjamin.herbomez@esial.net>
2019-06-03 08:17:52 +0000

    Change cors

--- app/main.ts
+++ app/main.ts
@@ -6,17 +6,10 @@ import { ConfigService } from './config/config.service';
 let config = new ConfigService();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: () => ({
-      origin: function (origin:any, callback:any) {
-        console.log(config.envConfig.cors);
-          if (config.envConfig.cors.indexOf(origin) !== -1 || origin == null) {
-              callback(null, true)
-          } else {
-              callback(new Error('Not allowed by CORS'))
-          }
-      }
-    })
+  const app = await NestFactory.create(AppModule)
+  });
+  app.enableCors({
+    origin:config.envConfig.cors
   });
   await app.listen(config.envConfig.port);
 }
