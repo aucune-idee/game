ec858ee97d4ad7fc5c5b2b6cfba656670373e825
Kamule <benjamin.herbomez@gmail.com>
2019-06-05 20:58:19 +0200

    Update games.module.ts

--- app/games/games.module.ts
+++ app/games/games.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { GamesController } from './controllers/games/games.controller';
-import { GameSchema, GameyCollectionName } from './schemas/game.schema';
+import { GamesController } from './controllers/games.controller';
+import { GameSchema, GameCollectionName } from './schemas/game.schema';
 import { GameGateway } from './gateway/game.gateway';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: GameyCollectionName, schema: GameSchema }])]
+  imports: [MongooseModule.forFeature([{ name: GameCollectionName, schema: GameSchema }])],
   controllers: [GamesController],
   providers: [GameGateway]
 })
