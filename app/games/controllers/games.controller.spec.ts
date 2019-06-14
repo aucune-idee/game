ecf98894158bc9d3905bd772607a4cced0a3b16a
Kamule <benjamin.herbomez@esial.net>
2019-06-14 12:10:14 +0000

    Fix

--- app/games/controllers/games.controller.spec.ts
+++ app/games/controllers/games.controller.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GamesController } from './games.controller';
 
+import { CreateGameService } from '../services/create-game/create-game.service';
+
+jest.mock('../services/create-game/create-game.service')
+
 describe('Games Controller', () => {
   let controller: GamesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GamesController],
+      providers:[
+        {provide:CreateGameService, useValue: new CreateGameService(null, null)},
+      ]
     }).compile();
 
     controller = module.get<GamesController>(GamesController);
---
+++ app/games/services/index.ts
@@ -0,0 +1,2 @@
+export * from 'create-game/create-game.service';
+export * from 'get-games/get-games.service';
